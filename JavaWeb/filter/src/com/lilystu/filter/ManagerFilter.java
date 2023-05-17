5e9b47e45b2867bc4be59c8f7e3ecb8da6570d8d
lilyOrio <91266959+lilyOrio@users.noreply.github.com>
2023-05-17 23:38:27 +0800

    [2023/5/17filter]

---
+++ JavaWeb/filter/src/com/lilystu/filter/ManagerFilter.java
@@ -0,0 +1,6 @@
+package com.lilystu.filter;/**
+*@author lily
+*@version 1.0
+*/
+   public class ManagerFilter {
+}
---
+++ JavaWeb/filter/src/com/lilystu/servlet/LoginCheckServlet.java
@@ -0,0 +1,14 @@
+/**
+*@author lily
+*@version 1.0
+*/
+   @javax.servlet.annotation.WebServlet(name = "LoginCheckServlet")
+public class LoginCheckServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+
+    }
+}
