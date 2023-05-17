5c06e977d8329f7ef5d3a674b080c1264c9b14df
lilyOrio <91266959+lilyOrio@users.noreply.github.com>
2023-05-17 23:40:02 +0800

    [2023/5/17filter]

--- JavaWeb/filter/src/com/lilystu/filter/ManagerFilter.java
+++ JavaWeb/filter/src/com/lilystu/filter/ManagerFilter.java
@@ -1,6 +1,34 @@
-package com.lilystu.filter;/**
-*@author lily
-*@version 1.0
-*/
-   public class ManagerFilter {
+package com.lilystu.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * @author lily
+ * @version 1.0
+ */
+public class ManagerFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // Filter被Tomcat创建时调用此方法，可以完成一些初始化动作
+        System.out.println("ManagerFilter init被调用。。。");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+//      每次调用该filter时，doFilter会被调用
+        System.out.println("ManagerFilter doFilter被调用。。。");
+//        如果这里没有调用继续请求的方法，则会在这里停止
+
+//        如果继续访问目标资源==》
+//        这里的request和response对象已经被Tomcat封装好了
+        filterChain.doFilter(servletRequest,servletResponse);//等价于放行
+    }
+
+    @Override
+    public void destroy() {
+
+    }
 }
--- JavaWeb/filter/src/com/lilystu/servlet/LoginCheckServlet.java
+++ JavaWeb/filter/src/com/lilystu/servlet/LoginCheckServlet.java
@@ -1,14 +1,37 @@
-/**
-*@author lily
-*@version 1.0
-*/
-   @javax.servlet.annotation.WebServlet(name = "LoginCheckServlet")
-public class LoginCheckServlet extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+package com.lilystu.servlet;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * @author lily
+ * @version 1.0
+ */
+@WebServlet(urlPatterns = "/login")
+public class LoginCheckServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("LoginCheckServlet");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if ("123456".equals(password)) {
+            HttpSession session = request.getSession();
+            session.setAttribute("username",username);
+            session.setAttribute("password",password);
+            request.getRequestDispatcher("/manager/admin.jsp")
+                    .forward(request, response);
+        } else {
+            request.getRequestDispatcher("/login.jsp")
+                    .forward(request, response);
+        }
     }
 
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
     }
 }
