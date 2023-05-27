6125f5ccf6e2726d38c47dc8ab5bbf3336aee9ad
uidq5850 <lili.li@desaysv.com>
2023-05-27 20:09:53 +0800

    [2023/5/27ajax入门案例]

--- JavaWeb/ajax/src/com/lilystu/servlet/CheckUserServlet.java
+++ JavaWeb/ajax/src/com/lilystu/servlet/CheckUserServlet.java
@@ -1,5 +1,8 @@
 package com.lilystu.servlet;
 
+import com.google.gson.Gson;
+import com.lilystu.entity.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,6 +17,15 @@ public class CheckUserServlet extends HttpServlet {
         //接受ajax提交的数据
         String username = request.getParameter("username");
         System.out.println("uname = " + username);
+
+        response.setContentType("text/html;charset=utf-8");
+        if ("king".equals(username)){
+            User king = new User(100, "king", "king@shou.com", "666");
+            String strKing = new Gson().toJson(king);
+            response.getWriter().write(strKing);
+        }else {
+            response.getWriter().write("");
+        }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
