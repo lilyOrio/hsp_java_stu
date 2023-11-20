05bebc78322ac30096f3fb1db0d28caca8796a0a
uidq5850 <lili.li@desaysv.com>
2023-11-20 08:19:37 +0800

    [23/11/16][提供XML配置AOP+作业2]

--- spring/spring/src/com/lily_stu/spring/aop/homework02/CallXMLAspect02.java
+++ spring/spring/src/com/lily_stu/spring/aop/homework02/CallXMLAspect02.java
@@ -11,12 +11,13 @@ import org.springframework.stereotype.Component;
 @Order(2)
 public class CallXMLAspect02 {
     //切入点表达式重用
-    @Pointcut(value = "execution(public int com.lily_stu.spring.aop.homework02.MyCal.*(int))")
+    @Pointcut(value = "execution(public int MyCal.*(int))")//切面类和实体类在一个包就可以直接写简单类名
     public void myPointCut() {
     }
 
     @Before(value = "myPointCut()")
     public void showBegin(JoinPoint joinPoint){
+        System.out.println("开始执行计算" + System.currentTimeMillis());
         System.out.println("CallXMLAspect02 前置通知~ 方法名：" + joinPoint.getSignature().getName());
     }
 
@@ -26,6 +27,7 @@ public class CallXMLAspect02 {
         Signature signature = joinPoint.getSignature();
 // 3. 在目标方法结束后打印“方法结束”日志
         System.out.println("CallXMLAspect02 ~~~日志--方法名：" + signature.getName() + "--方法正常结束 --~" + "返回值：" + res);
+        System.out.println("结束执行计算" + System.currentTimeMillis());
     }
 
     @AfterThrowing(value = "myPointCut()",throwing = "throwing")
