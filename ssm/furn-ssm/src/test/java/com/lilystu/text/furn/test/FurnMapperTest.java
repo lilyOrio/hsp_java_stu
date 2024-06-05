8fa185fb5127907d410d05f10b6ec6593e96933b
lili.li <lili.li@desaysv.com>
2024-06-05 22:00:06 +0800

    [24/6/4][SSM][环境搭建][.myBatis逆向工程]

---
+++ ssm/furn-ssm/src/test/java/com/lilystu/text/furn/test/FurnMapperTest.java
@@ -0,0 +1,20 @@
+package com.lilystu.text.furn.test;
+
+import org.junit.Test;
+
+public class FurnMapperTest {
+
+    @Test
+    public void insertSelective() {}
+
+    @Test
+    public void selectByExample() {}
+
+    @Test
+    public void deleteByPrimaryKey() {}
+
+    @Test
+    public void updateByPrimaryKey() {}
+
+
+}
