ce987358c4c9b6d71b79bdd217e58d7d7b5571bb
lily <1401372496@qq.com>
2024-06-10 22:48:13 +0800

    [24/6/10][SSM][搭建 Vue 前端工程][创建项目基础界面]

---
+++ ssm/vue/ssm_vue/src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <div style="height: 50px; line-height: 50px; border-bottom: 1px solid #ccc; display: flex">
+        <div style="width: 200px; padding-left: 30px; font-weight: bold; color: dodgerblue">后台管理</div>
+        <div style="flex: 1"></div>
+        <div style="width: 100px">
+            <div class="flex flex-wrap items-center">
+                <el-dropdown>
+                    <el-button type="primary">
+                       lily
+                    </el-button>
+                    <template #dropdown>
+                        <el-dropdown-menu>
+                            <el-dropdown-item>账户</el-dropdown-item>
+                            <el-dropdown-item>退出登录</el-dropdown-item>
+                        </el-dropdown-menu>
+                    </template>
+                </el-dropdown>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- ssm/vue/ssm_vue/src/views/HomeView.vue
+++ ssm/vue/ssm_vue/src/views/HomeView.vue
@@ -1,23 +1,56 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-<!--
-    在HomeView.vue引入HelloWorld组件
-    这里会显示HelloWorld组件的内容
-    msg="Welcome to Your Vue.js App"表示给组件HelloWorld设置了一个组件之
--->
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <!--    <img alt="Vue logo" src="../assets/logo.png">-->
+        <!--
+            在HomeView.vue引入HelloWorld组件
+            这里会显示HelloWorld组件的内容
+            msg="Welcome to Your Vue.js App"表示给组件HelloWorld设置了一个组件之
+        -->
+        <!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+
+        <div style="margin: 10px 0">
+            <el-button type="primary">新增</el-button>
+            <el-button>其它</el-button>
+        </div>
+
+        <!-- 搜索-->
+        <div style="margin: 10px 0">
+            <el-input v-model="search" placeholder=" 请 输 入 关 键 字 " style="width: 30%"></el-input>
+            <el-button style="margin-left: 10px" type="primary">查询</el-button>
+        </div>
+
+        <el-table :data="tableData" stripe style="width: 90%">
+            <el-table-column prop="date" label="日期"></el-table-column>
+            <el-table-column prop="name" label="姓名"></el-table-column>
+            <el-table-column prop="address" label="地址"></el-table-column>
+
+            <el-table-column fixed="right" label="操作" width="100">
+                <template #default="scope">
+                    <el-button @click="handleEdit(scope.row)" type="text">编辑</el-button>
+                    <el-button type="text">删除</el-button>
+                </template>
+            </el-table-column>
+
+        </el-table>
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'HomeView',
+        components: {},
+        data() {
+            return {
+                search: '',
+                tableData: [{date: '2016-05-02', name: '王小虎', address: '上海市普陀区金沙江路 1518 弄',},
+                    {date: '2016-05-04', name: '王小虎', address: '上海市普陀区金沙江路 1517 弄',},
+                    {date: '2016-05-01', name: '王小虎', address: '上海市普陀区金沙江路 1519 弄',}]
+            }
+        },
+        methods: {
+            handleEdit() {
+            }
+        }
+    }
 </script>
