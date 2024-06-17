fd5654488f7b9714d06f94571fb6963f1a6ba8ff
lili.li <lili.li@desaysv.com>
2024-06-17 21:26:32 +0800

    [24/6/15][SpringBoot][快速入门]

---
+++ lilystu_springmc/01_quickstart/src/main/java/com/lilystu/springboot/MainApp.java
@@ -0,0 +1,12 @@
+package com.lilystu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApp {
+    public static void main(String[] args) {
+        //启动SpringBoot 应用程序
+        SpringApplication.run(MainApp.class, args);
+    }
+}
