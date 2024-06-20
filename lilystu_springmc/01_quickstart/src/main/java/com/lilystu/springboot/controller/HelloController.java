5ae9a5d937a5bf808a58f5496b4cfe6919f7f593
lili.li <lili.li@desaysv.com>
2024-06-20 21:02:16 +0800

    [24/6/15][SpringBoot][修改默认配置][自定义配置]

--- lilystu_springmc/01_quickstart/src/main/java/com/lilystu/springboot/controller/HelloController.java
+++ lilystu_springmc/01_quickstart/src/main/java/com/lilystu/springboot/controller/HelloController.java
@@ -1,14 +1,18 @@
 package com.lilystu.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
+
+    @Value("${my.website}")
+    private String bdUrl;
     @RequestMapping("/hello")
     @ResponseBody
     public String hello(){
-        return "hello, spring boot";
+        return "hello, spring boot \n" + bdUrl;
     }
 }
