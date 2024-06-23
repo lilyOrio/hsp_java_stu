9502ef22d283c528acb603f4e6a8b18ce07b83c4
lily <1401372496@qq.com>
2024-06-23 21:56:14 +0800

    [24/6/23][SpringBoot][Springboot使用lombok]

--- lilystu_springboot/01_quickstart/src/main/java/com/lilystu/springboot/bean/Furn.java
+++ lilystu_springboot/01_quickstart/src/main/java/com/lilystu/springboot/bean/Furn.java
@@ -1,31 +1,40 @@
 package com.lilystu.springboot.bean;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix = "furn01")
+//@ToString//在编译时生成toString方法，默认生成无参构造器
+@Data
+@NoArgsConstructor//生成无参构造器,不希望其他含参构造器覆盖掉无参构造器就可以使用这个注解，没有无参构造器会影响容器注入
+@AllArgsConstructor//生成全参构造器
 public class Furn {
     private Integer id;
     private String name;
     private Double price;
 
-    public Integer getId() {
-        return id;
-    }
-    public void setId(Integer id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public Double getPrice() {
-        return price;
-    }
-    public void setPrice(Double price) {
-        this.price = price;
-    }
+//    public Integer getId() {
+//        return id;
+//    }
+//    public void setId(Integer id) {
+//        this.id = id;
+//    }
+//    public String getName() {
+//        return name;
+//    }
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//    public Double getPrice() {
+//        return price;
+//    }
+//    public void setPrice(Double price) {
+//        this.price = price;
+//    }
+
 }
--- lilystu_springboot/01_quickstart/src/main/java/com/lilystu/springboot/controller/HelloController.java
+++ lilystu_springboot/01_quickstart/src/main/java/com/lilystu/springboot/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.lilystu.springboot.controller;
 
 import com.lilystu.springboot.bean.Furn;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@Slf4j
 public class HelloController {
 
     @Value("${my.website}")
@@ -20,9 +22,11 @@ public class HelloController {
 
     @Autowired
     Furn furn;
+
     @RequestMapping("/furn")
     @ResponseBody
     public Furn furn(){
+        log.info("furn{}",furn);
         return furn;
     }
 }
