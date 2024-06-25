ec27e4d3e9f7d94f84bb4cb71cba54d458543d92
lili.li <lili.li@desaysv.com>
2024-06-25 15:47:59 +0800

    [24/6/25][SpringBoot][SpringBoot 转换器][内容协商]

--- lilystu_springboot/springbootweb/src/main/java/com/lilystu/springboot/controller/ResponseController.java
+++ lilystu_springboot/springbootweb/src/main/java/com/lilystu/springboot/controller/ResponseController.java
@@ -28,10 +28,18 @@ public class ResponseController {
         return monster;
 
         /*
+        AbstractJackson2HttpMessageConverter.java
         MediaType contentType = outputMessage.getHeaders().getContentType();//通过浏览器拿到返回的数据格式
         ObjectMapper objectMapper = this.selectObjectMapper(clazz, contentType);
         JsonGenerator generator = objectMapper.getFactory().createGenerator(outputStream, encoding);//获取对应的generator
         objectWriter.writeValue(generator, value);//将对象转换成对应格式
+         */
+
+        /*
+        内容协商：
+        服务器引入支持xml
+        浏览器请求头text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,·/*;q=0.
+            返回xml的权重比json高，默认返回xml格式数据，如果要返回json则可以设置format=json如：http://localhost:10001/getMonster?format=json
          */
     }
 }
