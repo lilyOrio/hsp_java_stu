0bf7af01398ab1b80ffcfc50293ead2e99c8cbd2
lily <1401372496@qq.com>
2024-06-27 22:54:50 +0800

    [24/6/27][SpringBoot][SpringBoot 异常处理][自定义异常页面实现]

---
+++ lilystu_springboot/springboot-usersys/src/main/java/com/lilystu/springboot/controller/MyErrorController.java
@@ -0,0 +1,22 @@
+package com.lilystu.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class MyErrorController {
+
+    @GetMapping("/err")
+    //模拟服务端异常
+    public String err(){
+        int i = 10/0;
+        return "manage";
+    }
+
+    @PostMapping("/err2")
+    //模拟客户端异常，客户端使用GET方式访问
+    public String err2(){
+        return "manage";
+    }
+}
