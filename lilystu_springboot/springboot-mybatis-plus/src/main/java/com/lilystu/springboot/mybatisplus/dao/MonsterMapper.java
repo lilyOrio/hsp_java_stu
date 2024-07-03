00cc7bb07167b9691b05eb2f779fe25c55ca5c79
lili.li <lili.li@desaysv.com>
2024-07-03 21:07:00 +0800

    [24/6/27][SpringBoot][SpringBoot 数据库操作][SpringBoot整合MyBatis-plus][MyBatisx插件的使用]

--- lilystu_springboot/springboot-mybatis-plus/src/main/java/com/lilystu/springboot/mybatisplus/dao/MonsterMapper.java
+++ lilystu_springboot/springboot-mybatis-plus/src/main/java/com/lilystu/springboot/mybatisplus/dao/MonsterMapper.java
@@ -1,4 +1,5 @@
 package com.lilystu.springboot.mybatisplus.dao;
+import org.apache.ibatis.annotations.Param;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.lilystu.springboot.mybatisplus.bean.Monster;
@@ -7,4 +8,8 @@ import org.apache.ibatis.annotations.Mapper;
 //@Mapper
 public interface MonsterMapper extends BaseMapper<Monster> {
     //这里可以继续添加需要的方法
+
+    int insertSelective(Monster monster);
+
+    int delByEmail(@Param("email") String email);
 }
