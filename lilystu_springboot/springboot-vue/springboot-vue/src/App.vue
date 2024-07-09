b86c9dbe4616a41abee9f9cd42edf9fe6c5c3ece
lily <1401372496@qq.com>
2024-07-09 22:42:27 +0800

    [24/7/9][SpringBoot+vue3][搭建 Vue 前端工程+创建项目基础界面]

---
+++ lilystu_springboot/springboot-vue/springboot-vue/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <!-- 头部-->
+        <Header/> <!-- 主体-->
+        <div style="display: flex"> <!-- 侧边栏-->
+            <Aside/> <!-- 内容区域,表格, 这个部分是从 HomeView.vue 组件来的-->
+            <router-view style="flex: 1"/>
+        </div>
+    </div>
+</template>
+<script>
+    import Header from "@/components/Header";
+    import Aside from "@/components/Aside";
+
+    export default {name: "Layout", components: {Header,Aside}}
+</script>
+<style>
+
+</style>
---
+++ lilystu_springboot/springboot-vue/springboot-vue/src/views/HomeView.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <div style="margin: 10px 0">
+            <el-button type="primary">新增</el-button>
+            <el-button>其它</el-button>
+        </div>
+        <!-- 搜索-->
+        <div style="margin: 10px 0">
+            <el-input v-model="search" placeholder=" 请 输 入 关 键 字 " style="width: 30%"></el-input>
+            <el-button style="margin-left: 10px" type="primary">查询</el-button>
+        </div>
+        <!-- <img alt="Vue logo" src="../assets/logo.png">--> <!-- <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+        <!-- <el-button>我的按钮</el-button> --> <!-- 去掉字段的 width, 让其自适应 -->
+        <el-table :data="tableData" stripe style="width: 100%">
+            <el-table-column prop="date" label="日期"></el-table-column>
+            <el-table-column prop="name" label="姓名"></el-table-column>
+            <el-table-column prop="address" label="地址"></el-table-column>
+            <el-table-column fixed="right" label="操作" width="100">
+                <template #default="scope">
+                    <el-button @click="handleEdit(scope.row)" type="text">编辑</el-button>
+                    <el-button type="text">删除</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: 'HomeView',
+        components: {},
+        data() {
+            return {
+                tableData: [{date: '2016-05-02', name: '王小虎', address: '上海市普陀区金沙江路 1518 弄',},
+                    {date: '2016-05-04', name: '王小虎', address: '上海市普陀区金沙江路 1517 弄',},
+                    {date: '2016-05-01', name: '王小虎', address: '上海市普陀区金沙江路 1519 弄',}],
+                search: '',
+            }
+        },
+        methods: {
+            handleEdit() {
+            }
+        }
+    }
+</script>
