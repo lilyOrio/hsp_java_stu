78605c00eb40009d874ea1ece82de2c01030e6a9
lily <1401372496@qq.com>
2024-07-15 22:13:18 +0800

    [24/7/15][SpringBoot+vue3][切换数据源]

---
+++ lilystu_springboot/springboot-vue/springboot-furn/src/main/java/com/lilystu/springboot/config/DataSourceConfig.java
@@ -0,0 +1,23 @@
+package com.lilystu.springboot.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@Configuration
+@Slf4j
+public class DataSourceConfig {
+
+    @ConfigurationProperties("spring.datasource")
+    @Bean
+    public DataSource dataSource() throws SQLException {
+        DruidDataSource druidDataSource = new DruidDataSource();
+        log.info("数据源={}", druidDataSource.getClass());
+        return druidDataSource;
+    }
+}
