2508a2e76f453d985b57b8db939218f784105673
lily <1401372496@qq.com>
2024-07-24 00:06:08 +0800

    [24/7/22][SpringCloud][SpringCloud Eureka 服务注册与发现]

--- spring-cloud/e-commerce-center/member-service-provider-10000/src/main/java/com/lilystu/springcloud/MemberApplication.java
+++ spring-cloud/e-commerce-center/member-service-provider-10000/src/main/java/com/lilystu/springcloud/MemberApplication.java
@@ -2,7 +2,10 @@ package com.lilystu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+//@EnableEurekaClient 将该程序标识为 EurekaClient
+@EnableEurekaClient
 @SpringBootApplication
 public class MemberApplication {
     public static void main(String[] args) {
