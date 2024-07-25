86066d6b502069ab518d1dcdbecb54b06733cae0
lily <1401372496@qq.com>
2024-07-25 21:31:29 +0800

    [24/7/25][SpringCloud][替换负载均衡算法-随机负载均衡]

--- spring-cloud/e-commerce-center/member-service-consumer-10080/src/main/java/com/lilystu/springcloud/MemberConsumerApplication.java
+++ spring-cloud/e-commerce-center/member-service-consumer-10080/src/main/java/com/lilystu/springcloud/MemberConsumerApplication.java
@@ -1,13 +1,17 @@
 package com.lilystu.springcloud;
 
+import com.lilystu.springcloud.config.RibbonRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @EnableEurekaClient
 @SpringBootApplication
 @EnableDiscoveryClient//启用服务发现功能
+//使用指定的 Ribbon 负载均衡算法规则
+@RibbonClient(name = "MEMBER_SERVICE_PROVIDER_URL", configuration = RibbonRule.class)
 public class MemberConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(MemberConsumerApplication.class, args);
---
+++ spring-cloud/e-commerce-center/member-service-consumer-10080/src/main/java/com/lilystu/springcloud/config/RibbonRule.java
@@ -0,0 +1,16 @@
+package com.lilystu.springcloud.config;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//配置自己的负载均衡算法
+@Configuration
+
+public class RibbonRule {
+    @Bean
+    public IRule myRibbonRule() {
+        return new RandomRule();//随机负载均衡算法
+    }
+}
