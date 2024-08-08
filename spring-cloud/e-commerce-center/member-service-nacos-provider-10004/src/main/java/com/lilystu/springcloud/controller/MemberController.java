9a8d475ea2c5895520b6e9b6e8bb89c534527e92
lily <1401372496@qq.com>
2024-08-08 22:48:02 +0800

    [24/7/25][SpringCloud][SpringCloud Alibaba Nacos 服务注册中心和配置中心][Sentinel流控规则实例-关联,WarmUp(预热),排队]

--- spring-cloud/e-commerce-center/member-service-nacos-provider-10004/src/main/java/com/lilystu/springcloud/controller/MemberController.java
+++ spring-cloud/e-commerce-center/member-service-nacos-provider-10004/src/main/java/com/lilystu/springcloud/controller/MemberController.java
@@ -57,4 +57,48 @@ public class MemberController {
             return Result.error("402", "ID= " + id + " 不存在");
         }
     }
+
+
+    //流量控制实例--关联
+
+    /**
+     * 当调用 member-service-nacos-provider-10004 的 /t2 API 接口时，如果 QPS 超过 1，
+     * 这 时调用 /t1 API 接口 直接接失败，抛异常. 老师梳理 /t2 是关联的资源 , 限流的资源是 /t1
+     * @return
+     */
+    @GetMapping(value = "/t1")
+    public Result t1() {
+        return Result.success("t1 执行");
+    }
+
+    @GetMapping(value = "/t2")
+    public Result t2() {
+        return Result.success("t2 执行");
+    }
+
+    //流量控制--warm up
+    /**
+     * 通常冷启动的过程系统允许通过的 QPS 曲线图(上图)
+     *  默认 coldFactor 为 3，即请求 QPS 从 threshold / 3 开始，经预热时长逐渐升至设 定的 QPS 阈值
+     *  这里的threshold 就是最终要达到的QPS阈值.
+     */
+
+    //流量控制--排队 削峰添谷
+    /**
+     * 1. 需求: 通过 Sentinel 实现 流量控制-排队
+     * 2. 调用 member-service-nacos-provider-10004 的 /t3 API 接口，将 QPS 设置为 1
+     * 3. 当调用 /t3 的 QPS 超过 1 时，不拒绝请求，而是排队等待, 依次执行
+     * 4. 当等待时间超过 10 秒，则为等待超时.
+     */
+    @GetMapping(value = "/t3")
+    public Result t3() {
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("enter getMemberById... 当 前 线 程 id=" +
+                Thread.currentThread().getId() + " 时间=" + new Date());
+        return Result.success("t3 执行");
+    }
 }
